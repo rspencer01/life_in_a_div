8b9eb2ae6fee10932193bb329fba7551fd17b798
Robert Spencer <me@robertandrewspencer.com>
2025-07-07 18:13:22 +0100

    Install build tools if not already installed
    We need wasm-opt and wasm-strip. The user could provide these, but if
    they haven't the ./build.sh script now will download and install them.

--- build.sh
+++ build.sh
@@ -2,24 +2,48 @@
 
 set -euo pipefail
 
+PATH=$PATH:./.buildtools/bin
+
 rm -rf target
 
-echo -e "\033[1;32mStep 0 / 3\033[0m Checking build requirements..."
-programs=("cargo" "wasm-strip" "wasm-opt")
-for prog in "${programs[@]}"; do
-  if ! command -v $prog &> /dev/null; then
-    echo -e "\033[1;31mError\033[0m      Missing ${prog}"
-    exit 1
-  fi
-done
+echo -e "\033[1;32mStep 0 / 3\033[0m   Checking build requirements..."
+if ! command -v cargo &> /dev/null; then
+  echo -e "\033[1;31mError\033[0m        Missing rust toolchain"
+  exit 1
+fi
+if ! command -v wasm-strip &> /dev/null; then
+  echo -e "\033[1;33mWarning\033[0m      Missing wasm-strip. Installing..."
+  mkdir -p .buildtools/bin
+  pushd .buildtools
+  git clone --recursive https://github.com/WebAssembly/wabt
+  cd wabt
+  git submodule update --init
+  mkdir build 
+  cd build
+  cmake ..
+  cmake --build .
+  popd
+  ln -s $(pwd)/.buildtools/wabt/bin/wasm-strip .buildtools/bin/
+fi
+if ! command -v wasm-opt &> /dev/null; then
+  echo -e "\033[1;33mWarning\033[0m      Missing wasm-opt. Installing..."
+  mkdir -p .buildtools/bin || true
+  pushd .buildtools
+  git clone https://github.com/WebAssembly/binaryen.git
+  cd binaryen
+  git submodule update --init
+  cmake . && make -j4
+  popd
+  ln -s $(pwd)/.buildtools/binaryen/bin/wasm-opt .buildtools/bin/
+fi
   
-echo -e "\033[1;32mStep 1 / 3\033[0m Building rust code..."
+echo -e "\033[1;32mStep 1 / 3\033[0m   Building rust code..."
 cargo build --target wasm32-unknown-unknown --release
 
-echo -e "\033[1;32mStep 2 / 3\033[0m Stripping symbols..."
+echo -e "\033[1;32mStep 2 / 3\033[0m   Stripping symbols..."
 wasm-strip target/wasm32-unknown-unknown/release/life.wasm
 
-echo -e "\033[1;32mStep 3 / 3\033[0m Optimising wasm..."
+echo -e "\033[1;32mStep 3 / 3\033[0m   Optimising wasm..."
 wasm-opt -o target/wasm32-unknown-unknown/life-opt.wasm -Oz target/wasm32-unknown-unknown/release/life.wasm
 
 echo -e "Final program is $(stat -c%s target/wasm32-unknown-unknown/life-opt.wasm) bytes in size"
